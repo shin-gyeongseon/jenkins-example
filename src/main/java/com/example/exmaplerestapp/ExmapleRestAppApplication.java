861ff7a1e807d7d568e22f5f2b4c80e1b3fc3e52
shinGyeongseon <tlsrid1119@gmail.com>
2025-03-04 23:51:09 +0900

    first commit

---
+++ src/main/java/com/example/exmaplerestapp/ExmapleRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.exmaplerestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExmapleRestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExmapleRestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exmaplerestapp/ExmapleRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exmaplerestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExmapleRestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
